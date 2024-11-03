39b589c28f698ebf1a51210e6b6732dd15fb95e1
Rene Rasmussen <rene069g@elevcampus.dk>
2024-11-03 16:54:49 +0100

    Outcommented DebugCode

--- TestPage/Pages/Privacy.cshtml.cs
+++ TestPage/Pages/Privacy.cshtml.cs
@@ -18,28 +18,30 @@ namespace TestPage.Pages
             _logger = logger;
         }
 
-        public async void OngetAsync()
+        public async void OnGetAsync()
         {
+
+            //ONLY USED FOR DEBUGGING OF TOKEN
             // Retrieve tokens
-            IdToken = await HttpContext.GetTokenAsync("id_token");
-            AccessToken = await HttpContext.GetTokenAsync("access_token");
+            //IdToken = await HttpContext.GetTokenAsync("id_token");
+            //AccessToken = await HttpContext.GetTokenAsync("access_token");
 
-            // Now you can decode and inspect these tokens as needed
-            if (IdToken != null)
-            {
-                // Decode and inspect the ID token
-                var handler = new JwtSecurityTokenHandler();
-                var idTokenDecoded = handler.ReadJwtToken(IdToken);
-                // Use 'idTokenDecoded.Claims' to view the claims
-            }
+            //// can decode and inspect these tokens as needed
+            //if (IdToken != null)
+            //{
+            //    // Decode and inspect the ID token
+            //    var handler = new JwtSecurityTokenHandler();
+            //    var idTokenDecoded = handler.ReadJwtToken(IdToken);
+            //    // Use 'idTokenDecoded.Claims' to view the claims
+            //}
 
-            if (AccessToken != null)
-            {
-                // Decode and inspect the Access token
-                var handler = new JwtSecurityTokenHandler();
-                var accessTokenDecoded = handler.ReadJwtToken(AccessToken);
-                // Use 'accessTokenDecoded.Claims' to view the claims
-            }
+            //if (AccessToken != null)
+            //{
+            //    // Decode and inspect the Access token
+            //    var handler = new JwtSecurityTokenHandler();
+            //    var accessTokenDecoded = handler.ReadJwtToken(AccessToken);
+            //    // Use 'accessTokenDecoded.Claims' to view the claims
+            //}
         }
     }
 }
