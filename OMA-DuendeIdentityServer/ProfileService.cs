9a99a7f7f1145a87f2170dc3bd9dc5663f6c1aa8
Rene Rasmussen <rene069g@elevcampus.dk>
2024-11-09 16:52:06 +0100

    Removed unnessesary comments

--- OMA-DuendeIdentityServer/ProfileService.cs
+++ OMA-DuendeIdentityServer/ProfileService.cs
@@ -17,20 +17,18 @@ namespace OMA_DuendeIdentityServer
 
         public async Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
-            // Get the user
+            
             var user = await _userManager.GetUserAsync(context.Subject);
             if (user == null)
                 return;
 
-            // Get the roles for the user
+            
             var roles = await _userManager.GetRolesAsync(user);
 
-            // Add roles as claims
+            
             var roleClaims = roles.Select(role => new Claim(JwtClaimTypes.Role, role));
 
   
-
-            // Include the existing claims and the new role claims
             var claims = context.Subject.Claims.ToList();
             claims.AddRange(roleClaims);
 
